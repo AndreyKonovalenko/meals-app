b0943a3b435c4e836044d7f77c06ebb8301f64b0
EC2 Default User <andreinew@mail.ru>
2019-09-05 08:04:00 +0000

    Added material design tabs for Android platfrom

--- src/navigation/MealsNavigator.js
+++ src/navigation/MealsNavigator.js
@@ -4,6 +4,7 @@ import { createStackNavigator } from 'react-navigation-stack';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
 import { createAppContainer } from 'react-navigation';
 import { Ionicons } from '@expo/vector-icons';
+import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs';
 
 import CategoriesScreens from '../screens/CategoriesScreen';
 import CategoryMealsScreen from '../screens/CategoryMealsScreen';
@@ -28,13 +29,14 @@ const MealsNavigator = createStackNavigator({
   }
 });
 
-const MealsFavTabNavigator = createBottomTabNavigator({
+const tabScreenConfig = {
   Meals: {
     screen: MealsNavigator,
     navigationOptions: {
       tabBarIcon: (tabInfo) => {
         return <Ionicons name='ios-restaurant' size={25} color={tabInfo.tintColor}/>;
-      }
+      },
+      tabBarColor: Colors.primary
     }
   },
   Favorites: {
@@ -43,13 +45,23 @@ const MealsFavTabNavigator = createBottomTabNavigator({
       tabBarLabel: 'Favorites!',
       tabBarIcon: (tabInfo) => {
         return <Ionicons name='ios-star' size={25} color={tabInfo.tintColor}/>;
-      }
+      },
+      tabBarColor: Colors.secondary
     }
   }
-}, {
-  tabBarOptions: {
-    activeTintColor: Colors.secondary
-  }
-});
+};
+
+const MealsFavTabNavigator = Platform.OS === 'android' ?
+  createMaterialBottomTabNavigator(tabScreenConfig, {
+    activeTintColor: 'white',
+    shifting: true
+  }) :
+  createBottomTabNavigator(tabScreenConfig, {
+
+  }, {
+    tabBarOptions: {
+      activeTintColor: Colors.secondary
+    }
+  });
 
 export default createAppContainer(MealsFavTabNavigator);
