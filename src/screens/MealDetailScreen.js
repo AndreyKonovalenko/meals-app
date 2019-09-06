66217c1066c9f1c5cbd5ecd35050e5e3cc2f2fb3
EC2 Default User <andreinew@mail.ru>
2019-09-06 12:34:45 +0000

    Added basic mealDetailScreen content

--- src/screens/MealDetailScreen.js
+++ src/screens/MealDetailScreen.js
@@ -1,22 +1,33 @@
 import React from 'react';
-import {View, Text, StyleSheet, Button} from 'react-native';
-import {MEALS} from '../data/dummy-data';
-import {HeaderButtons, Item} from 'react-navigation-header-buttons';
+import { View, Text, StyleSheet, Button, ScrollView, Image } from 'react-native';
+import { MEALS } from '../data/dummy-data';
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 import CustomHeaderButton from '../components/CustomHeaderButton';
+import DefaultText from '../components/DefaultText';
+
+const ListItem = props => {
+  return (<View style={styles.listItem}>
+    <DefaultText>{props.children}</DefaultText>
+    </View>);
+}
 
 const MealDetailScreen = props => {
   const mealId = props.navigation.getParam('mealId');
   const selectedMeal = MEALS.find(element => element.id === mealId);
+
   return (
-    <View style={styles.screen}>
-      <Text>{selectedMeal.title}</Text>
-      <Button
-        title='Go Back to Categories'
-        onPress={() => {
-          props.navigation.popToTop();
-        }}
-      />
-    </View>
+    <ScrollView>
+    <Image source={{uri: selectedMeal.imageUrl}} style={styles.image}/>
+      <View style={styles.details}>
+        <DefaultText>{selectedMeal.duration}m</DefaultText>
+        <DefaultText>{selectedMeal.complexity.toUpperCase()}</DefaultText>
+        <DefaultText>{selectedMeal.affordability.toUpperCase()}</DefaultText>
+      </View>
+      <Text style={styles.title}>Ingredients</Text>
+      {selectedMeal.ingredients.map(element => <ListItem key={element}>{element}</ListItem>)}
+      <Text style={styles.title}>Steps</Text>
+      {selectedMeal.steps.map(element => <ListItem key={element}>{element}</ListItem>)}
+    </ScrollView>
   );
 };
 
@@ -38,11 +49,28 @@ MealDetailScreen.navigationOptions = navigationData => {
 };
 
 const styles = StyleSheet.create({
-  screen: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
+  image: {
+    width: '100%',
+    height: 200
+  },
+  details: {
+    flexDirection: 'row',
+    padding: 15,
+    justifyContent: 'space-around'
+  },
+  title: {
+    fontFamily: 'open-sans-bold',
+    fontSize: 22,
+    textAlign: 'center'
+  },
+  listItem: {
+    marginVertical: 10,
+    marginHorizontal: 20,
+    borderColor: '#ccc',
+    borderWidth: 1,
+    padding: 10
   }
+
 });
 
 export default MealDetailScreen;
